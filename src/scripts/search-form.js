3002f2dcd912ede6c4624d74876fcbb21709f864
Kely Martinez <kely@devacademy.la>
2017-03-24 18:45:18 -0500

    Add functions for enable a button and for get data from json file

--- src/scripts/search-form.js
+++ src/scripts/search-form.js
@@ -1,3 +1,23 @@
 function searchForm(){
-    // code here
+    $(document).ready(function() {
+        $('#buttonSearch').prop('disabled', true);
+        $('#message').keyup(function() {
+            if($(this).val().length !=0) {
+                $('#buttonSearch').prop('disabled', false);
+            }
+            else {
+                $('#buttonSearch').prop('disabled', true);
+            }
+        });
+    });
+    var url, bookName;
+    url = 'books-schema.json';
+    $.getJSON(url, function(data) {
+        $(data.data).each(function() {
+            bookName = "<option>" + this.title + "</option>";
+            $('#myList').append(bookName);
+        });
+        var input = $('#message')[0];
+        new Awesomplete(input, {list: $('#myList')[0]});
+    });
 }
