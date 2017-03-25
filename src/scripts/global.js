1f14af4bce76749e04f359ba5c6ba5c84829896e
Kely Martinez <kely@devacademy.la>
2017-03-24 19:10:44 -0500

    Remove white spaces

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -26,7 +26,7 @@ function showContent() {
     url = 'books-schema.json';
     $.getJSON(url, function(data) {
         $(data.entities.saved).each(function() {
-            bookSaved = "<li class='myListElement'><div class='listElementContainer'><a href='"+ this.url +"'>" + this.label + "</a><div class='action'><a href='#' class='removeElementList'>Eliminar</a><span>|</span><a href='#'>Modificar</a></div></div></li>";
+            bookSaved = "<li class='myListElement pure-menu-item'><div class='listElementContainer'><a href='"+ this.url +"'>" + this.label + "</a><div class='action'><a href='#' class='removeElementList'>Eliminar</a><span>|</span><a href='#'>Modificar</a></div></div></li>";
             $('#myListSaved').append(bookSaved);
         });
         $('.myListElement .listElementContainer .action .removeElementList').on('click',function(){
